a76a8718fbc6f864ea2a3a173b8b33b60e0afaa6
kawallala <martin.araya.zavala@gmail.com>
2019-08-25 13:56:06 -0400

    fixed some documentation, fixed identation

--- test/model/units/SwordMasterTest.java
+++ test/model/units/SwordMasterTest.java
@@ -10,34 +10,34 @@ import static org.junit.jupiter.api.Assertions.assertNull;
  */
 public class SwordMasterTest extends AbstractTestUnit {
 
-  private SwordMaster swordMaster;
+    private SwordMaster swordMaster;
 
-  /**
-   * Set up the main unit that's going to be tested in the test set
-   */
-  @Override
-  public void setTestUnit() {
-    swordMaster = new SwordMaster(50, 2, field.getCell(0, 0));
-  }
+    /**
+     * Set up the main unit that's going to be tested in the test set
+     */
+    @Override
+    public void setTestUnit() {
+        swordMaster = new SwordMaster(50, 2, field.getCell(0, 0));
+    }
 
-  @Override
-  public void setTargetUnits() {
-    super.setTargetUnits();
-  }
+    @Override
+    public void setTargetUnits() {
+        super.setTargetUnits();
+    }
 
-  /**
-   * @return the current unit being tested
-   */
-  @Override
-  public IUnit getTestUnit() {
-    return swordMaster;
-  }
+    /**
+     * @return the current unit being tested
+     */
+    @Override
+    public IUnit getTestUnit() {
+        return swordMaster;
+    }
 
-  @Override
-  @Test
-  public void equipSwordTest() {
-    assertNull(swordMaster.getEquippedItem());
-    this.sword.equipTo(this.swordMaster);
-    assertEquals(sword, swordMaster.getEquippedItem());
-  }
+    @Override
+    @Test
+    public void equipSwordTest() {
+        assertNull(swordMaster.getEquippedItem());
+        this.sword.equipTo(this.swordMaster);
+        assertEquals(sword, swordMaster.getEquippedItem());
+    }
 }
