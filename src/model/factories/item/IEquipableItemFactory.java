cee702df7cdf2dac1dfeb8c20bce2bebfa57766d
kawallala <martin.araya.zavala@gmail.com>
2019-12-12 18:07:25 -0400

    fixed unnecesary changes

--- src/model/factories/item/IEquipableItemFactory.java
+++ src/model/factories/item/IEquipableItemFactory.java
@@ -4,7 +4,7 @@ import model.items.IEquipableItem;
 
 /**
  * This interface represents all the item crating factories of the game
- * <p>
+ *
  * There is a default value for all the parameters an item can have, and a user can set different parameters to
  * the item that will be created
  *
@@ -13,44 +13,42 @@ import model.items.IEquipableItem;
  */
 
 public interface IEquipableItemFactory {
-  /**
-   * Creates an item with the parameters stored in the factory
-   *
-   * @return The created item
-   */
-  IEquipableItem create();
+    /**
+     * Creates an item with the parameters stored in the factory
+     *
+     * @return The created item
+     */
+    IEquipableItem create();
 
-  /**
-   * Sets the default parameters for the item to be created with
-   */
-  void setDefaults();
+    /**
+     * Sets the default parameters for the item to be created with
+     */
+    void setDefaults();
 
-  /**
-   * Sets a new name for the item
-   *
-   * @param name the name of the item that will be created
-   */
-  void setName(String name);
+    /**
+     * Sets a new name for the item
+     *
+     * @param name the name of the item that will be created
+     */
+    void setName(String name);
 
-  /**
-   * Sets the power for the item that will be created
-   *
-   * @param power the power of the item that will be created
-   */
-  void setPower(int power);
+    /**
+     * Sets the power for the item that will be created
+     *
+     * @param power the power of the item that will be created
+     */
+    void setPower(int power);
 
-  /**
-   * Sets the minimum range of the item that will be created
-   * If the new minimum is greater than the previous maximum, the maximum gets changed too
-   *
-   * @param minimumRange the minimum range of the item that will be created
-   */
-  void setMinimumRange(int minimumRange);
+    /**
+     * Sets the minimum range of the item that will be created
+     *
+     * @param minimumRange the minimum range of the item that will be created
+     */
+    void setMinimumRange(int minimumRange);
 
-  /**
-   * Sets the maximum range of the item that will be created, the maximum range can not be less than the minimum range
-   *
-   * @param maximumRange the maximum range of the item that will be created
-   */
-  void setMaximumRange(int maximumRange);
+    /**
+     * Sets the maximum ranfe of the item that will be created
+     * @param maximumRange the maximum range of the item that will be created
+     */
+    void setMaximumRange(int maximumRange);
 }
--- test/model/factories/item/AbstractEquipableItemFactoryTest.java
+++ test/model/factories/item/AbstractEquipableItemFactoryTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Assertions;
 
 /**
  * This class represents an Abstract test
- *
+ * <p>
  * The common behaviour of the tests are defined here
  */
 public class AbstractEquipableItemFactoryTest implements IEquipableItemFactoryTest {
   private IEquipableItemFactory factory;
-
+  private IEquipableItem item;
   @Override
   public void createTest() {
   }
@@ -28,17 +28,25 @@ public class AbstractEquipableItemFactoryTest implements IEquipableItemFactoryTe
   @Override
   public void setPowerTest() {
     factory.setPower(200);
-    IEquipableItem item = factory.create();
+    item = factory.create();
     Assertions.assertEquals(200, item.getPower());
   }
 
   @Override
   public void setMinimumRangeTest() {
-    factory.setMinimumRange();
+    factory.setMinimumRange(1);
+    item = factory.create();
+    Assertions.assertEquals(1, item.getMinRange());
+    factory.setMinimumRange(100);
+    item = factory.create();
+    Assertions.assertEquals(100, item.getMinRange());
+    Assertions.assertEquals(100, item.getMaxRange());
   }
 
   @Override
   public void setMaximumRangeTest() {
-
+    factory.setMaximumRange(4);
+    item = factory.create();
+    Assertions.assertEquals(4,item.getMaxRange());
   }
 }
